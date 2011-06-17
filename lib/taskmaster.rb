5658e9d1833f286b8448658208982b6969dd2ffa
Ben Scofield <git@turrean.com>
2011-06-17 11:15:04 -0400

    if in a Rails app, pull in all the models so we can tell which are henching

--- lib/taskmaster.rb
+++ lib/taskmaster.rb
@@ -4,6 +4,11 @@ require 'taskmaster/henchman'
 
 module Taskmaster
   def self.cron_output
+    # make sure all the models that could include Henchman are loaded
+    if defined?(Rails)
+      Dir[Rails.root.join('app', 'models', '**', '*.rb')].each { |file| require file }
+    end
+
     Henchman.included_in.map { |klass| klass.cron_output.strip }.join("\n")
   end
 end
